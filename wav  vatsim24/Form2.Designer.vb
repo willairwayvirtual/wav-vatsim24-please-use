8764d364c5a208c0614c664d5e29bf68fda5660d
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2022-02-18 14:37:32 +0000

    bsfmxkglgeaszujrqw

--- wav  vatsim24/Form2.Designer.vb
+++ wav  vatsim24/Form2.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form2
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,10 +20,11 @@ Partial Class Form2
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.connect = New System.Windows.Forms.Button()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
@@ -37,14 +38,14 @@ Partial Class Form2
         Me.TextBox1.Size = New System.Drawing.Size(252, 32)
         Me.TextBox1.TabIndex = 0
         '
-        'Button1
+        'connect
         '
-        Me.Button1.Location = New System.Drawing.Point(1184, 7)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(138, 32)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.connect.Location = New System.Drawing.Point(1064, 7)
+        Me.connect.Name = "connect"
+        Me.connect.Size = New System.Drawing.Size(263, 32)
+        Me.connect.TabIndex = 1
+        Me.connect.Text = "connect"
+        Me.connect.UseVisualStyleBackColor = True
         '
         'TextBox2
         '
@@ -56,11 +57,11 @@ Partial Class Form2
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(1454, 543)
+        Me.Button2.Location = New System.Drawing.Point(1381, 543)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(181, 75)
         Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "send messge"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'TextBox3
@@ -68,19 +69,21 @@ Partial Class Form2
         Me.TextBox3.Location = New System.Drawing.Point(-1, 543)
         Me.TextBox3.Multiline = True
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(1439, 174)
+        Me.TextBox3.Size = New System.Drawing.Size(1376, 174)
         Me.TextBox3.TabIndex = 4
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1631, 611)
+        Me.ClientSize = New System.Drawing.Size(1566, 618)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.connect)
         Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form2"
         Me.Text = "Form2"
         Me.ResumeLayout(False)
@@ -89,7 +92,7 @@ Partial Class Form2
     End Sub
 
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents connect As Button
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Button2 As Button
     Friend WithEvents TextBox3 As TextBox
--- wav  vatsim24/Form2.vb
+++ wav  vatsim24/Form2.vb
@@ -1,3 +1,58 @@
-﻿Public Class Form2
+﻿'Imports System.Net.Sockets
+Imports System.Text
+Public Class Form2
+    Dim clientSocket As New System.Net.Sockets.TcpClient()
+    Dim serverStream As NetworkStream
+    Dim readData As String
+    Dim infiniteCounter As Integer
 
+    Private Sub Connect_Click(ByVal sender As System.Object,
+        ByVal e As System.EventArgs) Handles connect.Click
+        Dim outStream As Byte() =
+        System.Text.Encoding.ASCII.GetBytes(TextBox2.Text + "$")
+        serverStream.Write(outStream, 0, outStream.Length)
+        serverStream.Flush()
+    End Sub
+
+    Private Sub msg()
+        If Me.InvokeRequired Then
+            Me.Invoke(New MethodInvoker(AddressOf msg))
+        Else
+            TextBox1.Text = TextBox1.Text +
+             Environment.NewLine + " >> " + readData
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object,
+    ByVal e As System.EventArgs) Handles Button2.Click
+        readData = "Conected to Chat Server ..."
+        msg()
+        clientSocket.Connect("127.0.0.1", 8888)
+        'Label1.Text = "Client Socket Program - Server Connected ..."
+        serverStream = clientSocket.GetStream()
+
+        Dim outStream As Byte() =
+        System.Text.Encoding.ASCII.GetBytes(TextBox3.Text + "$")
+        serverStream.Write(outStream, 0, outStream.Length)
+        serverStream.Flush()
+
+        Dim ctThread As Threading.Thread =
+         New Threading.Thread(AddressOf getMessage)
+        ctThread.Start()
+    End Sub
+
+    Private Sub getMessage()
+        For infiniteCounter = 1 To 2
+            infiniteCounter = 1
+            serverStream = clientSocket.GetStream()
+            Dim buffSize As Integer
+            Dim inStream(10024) As Byte
+            buffSize = clientSocket.ReceiveBufferSize
+            serverStream.Read(inStream, 0, buffSize)
+            Dim returndata As String =
+            System.Text.Encoding.ASCII.GetString(inStream)
+            readData = "" + returndata
+            msg()
+        Next
+    End Sub
 End Class
